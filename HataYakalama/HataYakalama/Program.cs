efd5805ab579ab0a0b2451d43c4d35733c66ff8d
yavuzgedik <yavuzgedik@outlook.com>
2016-12-08 11:15:17 +0200

    TryCatch

---
+++ HataYakalama/HataYakalama/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HataYakalama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] array = new int[4];
+            //Console.WriteLine(array[5]);
+
+            //try
+            //{
+            //    int[] array = new int[4];
+            //    Console.WriteLine(array[5]);
+            //}
+            //catch (IndexOutOfRangeException ex)
+            //{
+            //    //Console.WriteLine(ex.Message);
+            //    Console.WriteLine("Böyle bir dizin yok");
+            //}
+
+            //try
+            //{
+            //    int sayi = int.Parse(Console.ReadLine());
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Finally çalıştı.");
+            //}
+
+            //try
+            //{
+            //    int sayi = int.Parse(Console.ReadLine());
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //catch(OverflowException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("finally çalıştı.");
+            //}
+
+            //try
+            //{
+            //    int sayi = int.Parse(Console.ReadLine());
+            //}
+            //catch (Exception ex)
+            //{
+            //    
+            //}
+
+            try
+            {
+                throw new IndexOutOfRangeException();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
