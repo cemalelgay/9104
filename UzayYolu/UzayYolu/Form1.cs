d4781fed9294491b0bc94537fbd9ca6e9184566c
yavuzgedik <yavuzgedik@outlook.com>
2016-12-12 11:33:14 +0200

    Updated

---
+++ UzayYolu/UzayYolu/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UzayYolu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            rocket.Visible = false;
+        }
+
+        int genislik, yukseklik;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            genislik = Form1.ActiveForm.Width;
+            //yukseklik = Form1.ActiveForm.Height;
+
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                case Keys.A:
+                    SpaceShip.Left -= 10;
+                    break;
+
+                case Keys.Right:
+                case Keys.D:
+                    SpaceShip.Left += 10;
+                    break;
+
+                case Keys.Up:
+                case Keys.W:
+                    SpaceShip.Top -= 10;
+                    break;
+
+                case Keys.Down:
+                case Keys.S:
+                    SpaceShip.Top += 10;
+                    break;
+
+                case Keys.Space:
+                    int x = SpaceShip.Location.X + SpaceShip.Width / 2;
+                    int y = SpaceShip.Location.Y + SpaceShip.Height / 2;
+                    rocket.Location = new Point(x, y);
+                    rocket.Visible = true;
+                    timer1.Start();
+                    break;
+                    
+                default:
+                    break;
+            }
+
+            int SpaceX = SpaceShip.Location.X;
+            int SpaceY = SpaceShip.Location.Y;
+
+            if (SpaceX > genislik - SpaceShip.Width || SpaceX <= 0)
+            {
+                MessageBox.Show("Lanet olsun! Oyun Bitti.");
+                SpaceShip.Location = new Point(10, 10);
+                Application.Exit();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            genislik = Form1.ActiveForm.Width;
+            yukseklik = Form1.ActiveForm.Height;
+
+            if (rocket.Location.X < genislik - rocket.Width*2)
+            {
+                rocket.Left += 10;
+            }
+            else
+            {
+                timer1.Stop();
+            }
+            
+        }
+    }
+}
