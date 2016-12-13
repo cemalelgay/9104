1d89448b7bd7ea23a3063af60d4feaeb968eade1
yavuzgedik <yavuzgedik@outlook.com>
2016-12-13 09:28:15 +0200

    Updated

---
+++ Gecisler/Gecisler/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Gecisler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtMesaj = new System.Windows.Forms.TextBox();
+            this.BtnGonder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtMesaj
+            // 
+            this.TxtMesaj.Location = new System.Drawing.Point(75, 49);
+            this.TxtMesaj.Name = "TxtMesaj";
+            this.TxtMesaj.Size = new System.Drawing.Size(100, 20);
+            this.TxtMesaj.TabIndex = 0;
+            // 
+            // BtnGonder
+            // 
+            this.BtnGonder.Location = new System.Drawing.Point(88, 75);
+            this.BtnGonder.Name = "BtnGonder";
+            this.BtnGonder.Size = new System.Drawing.Size(75, 23);
+            this.BtnGonder.TabIndex = 1;
+            this.BtnGonder.Text = "Gönder";
+            this.BtnGonder.UseVisualStyleBackColor = true;
+            this.BtnGonder.Click += new System.EventHandler(this.BtnGonder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 159);
+            this.Controls.Add(this.BtnGonder);
+            this.Controls.Add(this.TxtMesaj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtMesaj;
+        private System.Windows.Forms.Button BtnGonder;
+    }
+}
+
