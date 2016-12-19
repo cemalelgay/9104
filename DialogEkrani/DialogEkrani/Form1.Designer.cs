e5010419027fbda8f8f0f5973dc2a95ef248bde1
yavuzgedik <yavuzgedik@outlook.com>
2016-12-19 11:55:52 +0200

    Added DialogResult

---
+++ DialogEkrani/DialogEkrani/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace DialogEkrani
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.BackColor = System.Drawing.SystemColors.Info;
+            this.BtnKaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnKaydet.Location = new System.Drawing.Point(15, 61);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(223, 31);
+            this.BtnKaydet.TabIndex = 0;
+            this.BtnKaydet.Text = "Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = false;
+            this.BtnKaydet.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(15, 35);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(223, 20);
+            this.TxtAd.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ad Soyad";
+            // 
+            // ListBox
+            // 
+            this.ListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.ItemHeight = 16;
+            this.ListBox.Location = new System.Drawing.Point(15, 98);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(219, 212);
+            this.ListBox.TabIndex = 3;
+            this.ListBox.DoubleClick += new System.EventHandler(this.ListBox_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(15, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Tümünü Sil";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 351);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.BtnKaydet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox ListBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DialogEkrani/DialogEkrani/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DialogEkrani
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (TxtAd.Text != "")
+            {
+                ListBox.Items.Add(TxtAd.Text);
+            }
+
+            TxtAd.Clear(); //TxtAd.Text = "";
+            TxtAd.Focus();
+            TxtAd.ForeColor = Color.Purple;
+        }
+
+        private void ListBox_DoubleClick(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Silmek İstiyor Musun?", "Onay Ekranı", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                ListBox.Items.RemoveAt(ListBox.SelectedIndex);
+            }
+            else if (result == DialogResult.No)
+            {
+
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Hepsi Silincek?", "Onay Ekranı", MessageBoxButtons.OKCancel);
+
+            if (result == DialogResult.OK)
+            {
+                ListBox.Items.Clear();
+            }
+            
+        }
+    }
+}
