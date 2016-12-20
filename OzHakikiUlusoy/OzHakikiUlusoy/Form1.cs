fe304906948a0761c74d9db61c3ef42f51ed0d43
yavuzgedik <yavuzgedik@outlook.com>
2016-12-20 10:31:36 +0200

    Updated

---
+++ OzHakikiUlusoy/OzHakikiUlusoy/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OzHakikiUlusoy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Musteri> MusteriListesi = new List<Musteri>();
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            Musteri musteri = new Musteri();
+            musteri.AdSoyAd = TxtMusteriAd.Text;
+            musteri.Cinsiyet = RadioBtnErkek.Checked == false ? true : false;
+            musteri.OtobusTuru = CBoxOtobusTuru.SelectedItem.ToString();
+
+            MusteriListesi.Add(musteri);
+            LBoxMusteriler.Items.Add(musteri);
+
+            TxtMusteriAd.Clear();
+            TxtMusteriAd.Focus();
+        }
+
+        private void LBoxMusteriler_DoubleClick(object sender, EventArgs e)
+        {
+            LBoxMusteriler.Items.RemoveAt(LBoxMusteriler.SelectedIndex);
+        }
+    }
+}
