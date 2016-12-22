ebd1b72bb96a63913b69d8188c2f1e8908e623bc
yavuzgedik <yavuzgedik@outlook.com>
2016-12-22 10:36:32 +0200

    Added Ctor

---
+++ Constructor/Constructor/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Constructor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void BtnOlustur_Click(object sender, EventArgs e)
+        {
+            string yeniPozisyon = CBoxPozisyon.SelectedItem.ToString();
+
+            Oyuncu oyuncu3 = new Oyuncu();
+
+            MessageBox.Show(oyuncu3.adi + " "  + oyuncu3.formaNo);
+
+            Oyuncu _oyuncu = new Oyuncu("Şehmuz", "Altay");
+
+            MessageBox.Show(_oyuncu.adi + " " + _oyuncu.takimi + " " + _oyuncu.formaNo);
+
+            Oyuncu oyuncu2 = new Oyuncu("Şehmuz", "Altay", yeniPozisyon, 9);
+
+            MessageBox.Show(oyuncu2.adi + " " + oyuncu2.takimi + " " + oyuncu2.pozisyonu + " " + oyuncu2.formaNo);
+
+        }
+    }
+}
---
+++ Constructor/Constructor/Oyuncu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Oyuncu
+    {
+        public string adi;
+        public string takimi;
+        public byte formaNo;
+        public string pozisyonu;
+
+        public Oyuncu()
+        {
+            adi = "A";
+            formaNo = 1;
+        }
+        public Oyuncu(string ad)
+        {
+            adi = ad;
+        }
+        public Oyuncu(string ad, string takim)
+        {
+            adi = ad;
+            takimi = takim;
+        }
+
+        public Oyuncu(string ad, string takim, string pozisyon, byte fNo)
+        {
+            adi = ad;
+            takimi = takim;
+            pozisyonu = pozisyon;
+            formaNo = fNo;
+        }
+    }
+}
