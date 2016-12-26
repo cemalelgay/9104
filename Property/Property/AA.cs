f7a0631ee3f2d2832bf041a7f5eb5212560543b1
yavuzgedik <yavuzgedik@outlook.com>
2016-12-26 09:43:47 +0200

    Ozan K.

---
+++ Property/Property/AA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property
+{
+    public class AA
+    {
+        public int X;
+        public int Y { get; set; }
+
+        private int _z;
+        public string mesaj = String.Empty;
+        public int Z
+        {
+            get
+            {
+                return _z;
+            }
+            set
+            {
+                //_z = value;
+                if (value > 81 || value < 1)
+                {
+                    mesaj = "Yanlış Plaka Kodu";
+                }
+                else
+                {
+                    _z = value;
+                }
+            }
+        }
+    }
+}
---
+++ Property/Property/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA nesne = new AA();
+            nesne.Z = 85;
+            
+            if (nesne.mesaj == null)
+            {
+                Console.WriteLine(nesne.Z);
+            }
+            else
+            {
+                Console.WriteLine(nesne.mesaj);
+            }
+            
+        }
+    }
+}
