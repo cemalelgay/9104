347162a0d5344b0f14d6bd11eacdab5f4bd45dca
yavuzgedik <yavuzgedik@outlook.com>
2016-12-26 11:45:54 +0200

    Cemal K.

---
+++ Araba_OOP/Araba_OOP/Araba.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Araba_OOP
+{
+    class Araba
+    {
+        public int yakitDepoKapasite { get; set; }
+        public double ortalamaTuketim100km { get; set; }
+        public static double yakitFiyat { get; set; }
+        public double toplamYol
+        {
+            get
+            {
+                return (yakitDepoKapasite / ortalamaTuketim100km) * 100;
+            }
+        }
+
+        public double UcretHesaplama(double km)
+        {
+            return km * (ortalamaTuketim100km / 100) * yakitFiyat;
+        }
+
+        public Araba(int yakitKapasite, double ortTuk100km)
+        {
+            this.ortalamaTuketim100km = ortTuk100km;
+            this.yakitDepoKapasite = yakitKapasite;
+        }
+    }
+}
